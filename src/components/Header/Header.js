540ab465080b9947481d9c3428007c0be2a7d3e1
Jeffrey Platt <jsplatt@gmail.com>
2021-07-26 14:39:46 -0400

    break

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,7 @@ function Header() {
     return (
         <div className='header'>
             <div className='header__top'>
+                <img src='https://images.squarespace-cdn.com/content/v1/60aebfd53942b43aa5e17c42/1623294976419-9U1XSYK93JLP0HXIMHRX/CCNY%2Blong%2BLogo.jpg?format=1500w' alt='logo'/>
                 <h1>Hello, Username!</h1>
             </div>
         </div>
