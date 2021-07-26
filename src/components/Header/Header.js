b8087e9a53b133a3f2bda8ca2d3175b1ed8b812a
Jeffrey Platt <jsplatt@gmail.com>
2021-07-26 15:11:17 -0400

    initial style of header done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,13 +6,13 @@ function Header() {
         <div className='header'>
             <div className='header__top'>
                 <img src='https://images.squarespace-cdn.com/content/v1/60aebfd53942b43aa5e17c42/1623294976419-9U1XSYK93JLP0HXIMHRX/CCNY%2Blong%2BLogo.jpg?format=1500w' alt='logo'/>
-                <h1>WELCOME, USER!</h1>
+                <h1> 🌿 WELCOME, USER! 🌿 </h1>
             </div>
             <div className='header__links'>
-                <h2>|  DASHBOARD  |</h2>
-                <h2>|  TASK MANAGEMENT  |</h2>
-                <h2>|  DOCUMENT CENTER  |</h2>
-                <h2>|  VENDOR TRACKER  |</h2>
+                <h2>|  DASHBOARD  |</h2> 
+                <h2>|  TASK MANAGEMENT  |</h2> 
+                <h2>|  DOCUMENT CENTER  |</h2> 
+                <h2>|  VENDOR TRACKER  |</h2> 
                 <h2>|  NOTES  |</h2>
             </div>
         </div>
