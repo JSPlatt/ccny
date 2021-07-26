d41f5e1aae79f06673c23664408397577c69d8a9
Jeffrey Platt <jsplatt@gmail.com>
2021-07-26 14:12:47 -0400

    initial design started

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import HeaderLinks from '../HeaderLinks/HeaderLinks'
 import './Header.css'
 
 function Header() {
     return (
         <div className='header'>
-            <h1>This is the Header Component</h1>
+            <div className='header__top'>
+                <h1>Hello, Username!</h1>
+            </div>
+            <HeaderLinks/>
         </div>
     )
 }
---
+++ src/components/HeaderLinks/HeaderLinks.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function HeaderLinks() {
+    return (
+        <div className='header__links'>
+            <h1>Link 1 |</h1> <br/>
+            <h1>Link 2 |</h1> <br/>
+            <h1>Link 3 |</h1> <br/>
+            <h1>Link 4 |</h1> <br/>
+            <h1>Link 5 |</h1> <br/>
+            <h1>Link 6 |</h1> <br/>
+        </div>
+    )
+}
+
+export default HeaderLinks
