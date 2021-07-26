f2241cfec87fd0177b7523d7073fe6671433984b
Jeffrey Platt <jsplatt@gmail.com>
2021-07-26 14:54:53 -0400

    ...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Header from './components/Header/Header';
-import HeaderLinks from './components/HeaderLinks/HeaderLinks';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      {/* <HeaderLinks/> */}
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,15 @@ function Header() {
         <div className='header'>
             <div className='header__top'>
                 <img src='https://images.squarespace-cdn.com/content/v1/60aebfd53942b43aa5e17c42/1623294976419-9U1XSYK93JLP0HXIMHRX/CCNY%2Blong%2BLogo.jpg?format=1500w' alt='logo'/>
-                <h1>Hello, Username!</h1>
+                <h1>Hello, User!</h1>
+            </div>
+            <div className='header__links'>
+                <h2>| Link |</h2>
+                <h2>| Link |</h2>
+                <h2>| Link |</h2>
+                <h2>| Link |</h2>
+                <h2>| Link |</h2>
+                <h2>| Link |</h2>
             </div>
         </div>
     )
