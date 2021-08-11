33a6c4590e8f50ec65c39c3fc2882d26cab056b5
Jeffrey Platt <jsplatt@gmail.com>
2021-08-11 13:12:31 -0400

    quick update

--- src/components/Dashboard/ToDo.js
+++ src/components/Dashboard/ToDo.js
@@ -3,13 +3,13 @@ import React from 'react'
 function ToDo() {
     return (
         <div>
-            <ul>
-                <h3>TODO-1</h3>
-                <h3>TODO-2</h3>
-                <h3>TODO-3</h3>
-                <h3>TODO-4</h3>
-                <h3>TODO-5</h3>
-            </ul>
+            <ol>
+                <li><h3>TODO-1</h3></li>
+                <li><h3>TODO-2</h3></li>
+                <li><h3>TODO-3</h3></li>
+                <li><h3>TODO-4</h3></li>
+                <li><h3>TODO-5</h3></li>
+            </ol>
         </div>
     )
 }
