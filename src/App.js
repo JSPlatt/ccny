6a65fd89f8cefccbf48af6ebe8437ff76a77a32a
Jeffrey Platt <jsplatt@gmail.com>
2021-08-12 14:39:30 -0400

    router added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Dashboard from './components/Dashboard/Dashboard';
 import Header from './components/Header/Header';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
+  <Router>
     <div className="App">
       <Header/>
       <Dashboard/>
     </div>
+  </Router>
   );
 }
 
