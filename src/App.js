370feb01feea8435af3e673ed16c66a86a3e2419
Jeffrey Platt <jsplatt@gmail.com>
2021-09-10 14:18:46 -0400

    adding routes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,35 @@ import './App.css';
 import Dashboard from './components/Dashboard/Dashboard';
 import Header from './components/Header/Header';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import TaskManagement from './components/Header/Task Management/TaskManagement';
+import Notes from './components/Header/Notes/Notes';
+import VendorTracker from './components/Header/Vendor Tracker/VendorTracker'
+import DocumentCenter from './components/Header/Document Center/DocumentCenter';
 
 function App() {
   return (
-  <Router>
+    <Router>
+    <Header/>
     <div className="App">
-      <Header/>
       <Dashboard/>
+    <Switch>
+      <Route path="/" component={Dashboard}/>
+      <Route path="/tasks" component={TaskManagement}/>
+      <Route path="/notes" component={Notes}/>
+      <Route path="/vendortracker" component={VendorTracker}/>
+      <Route path="/documentcenter" component={DocumentCenter}/>
+    </Switch>
     </div>
   </Router>
+
   );
 }
 
 export default App;
+
+{/* <Switch>
+      <Route exact path="/login" component={Login}/>
+      <Route exact path="/menu" component={Menu}/>
+      <Route exact path="/" component={StoresContainer}/>
+      <Route exact path="/stores/:slug" component={Store}/>
+    </Switch> */}
