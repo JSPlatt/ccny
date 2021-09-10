206266257d6d9c786be45ae48df53dbdebcfdb14
Jeffrey Platt <jsplatt@gmail.com>
2021-09-10 13:51:58 -0400

    adding files

---
+++ src/components/Header/Document Center/DocumentCenter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DocumentCenter() {
+    return (
+        <div>
+            <h1>This is the Document Center</h1>
+        </div>
+    )
+}
+
+export default DocumentCenter
---
+++ src/components/Header/Notes/Notes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Notes() {
+    return (
+        <div>
+            <h1>This is the Notes Section</h1>
+        </div>
+    )
+}
+
+export default Notes
