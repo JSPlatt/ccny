84c804def5054c7662ea8d5b46034503b5ae5936
Jeffrey Platt <jsplatt@gmail.com>
2021-09-10 13:53:26 -0400

    HeaderLink Files added

---
+++ src/components/Header/Task Management/TaskManagement.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TaskManagement() {
+    return (
+        <div>
+            <h1>This is TaskManagement</h1>
+        </div>
+    )
+}
+
+export default TaskManagement
---
+++ src/components/Header/Vendor Tracker/VendorTracker.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function VendorTracker() {
+    return (
+        <div>
+            <h1>This is the Vendor Tracker</h1>
+        </div>
+    )
+}
+
+export default VendorTracker
